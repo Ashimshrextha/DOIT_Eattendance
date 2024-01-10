e7e46a6f0910122ef1c03cc4503f824f3d0f0f52
dinesh bajgain <bajgaindinesh@gmail.com>
2024-01-10 08:01:36 +0545

    added graph for super admin

--- AttendanceManagementSystem/Scripts/Menu/customChart.js
+++ AttendanceManagementSystem/Scripts/Menu/customChart.js
@@ -1,4 +1,5 @@
 ﻿function loadMyChartData(chartData) {
+    debugger;
     const data = chartData.map(res => ({ LeaveTitle: res.LeaveTitle, RemainingLeave: res.RemainingLeave, AvailableLeave: res.AvailableLeave }));
     const chartLabels = chartData.map(res => res.LeaveTitle)
     const ctx = document.getElementById('myChart');
@@ -22,6 +23,58 @@
                     label: 'कुल बिदा',
                     data: data.map(item => item.AvailableLeave),
                   
+                }
+            ]
+        },
+        options: {
+            maintainAspectRatio: false,
+            responsive: !0,
+            barThickness: 35,
+            plugins: {
+                legend: {
+                    display: !1
+                }
+            },
+            scales: {
+                xAxes: [{
+                    gridLines: {
+                        color: "rgba(0, 0, 0, 0)"
+                    }
+                }],
+                y: {
+                    min: 0
+                }
+            }
+        }
+    });
+}
+var count = 0
+function loadSuperAdminChartData(chartData) {
+    count = count+1
+    console.log(count)
+    const data = chartData.map(res => ({ LeaveTitle: res.LeaveTitle, RemainingLeave: res.RemainingLeave, AvailableLeave: res.AvailableLeave }));
+    const chartLabels = chartData.map(res => res.LeaveTitle)
+    const ctx = document.getElementById('superadminChart');
+    new Chart(ctx, {
+        height: 30,
+        width: 100,
+        type: 'bar',
+        data: {
+            labels: chartLabels,
+            datasets: [
+                {
+                    borderWidth: 2,
+                    label: 'बाँकी बिदा',
+                    backgroundColor: "rgba(246, 39, 53, 1)",
+                    data: data.map(item => item.RemainingLeave),
+
+                },
+                {
+                    borderWidth: 2,
+                    backgroundColor: "rgba(254, 193, 32, 1)",
+                    label: 'कुल बिदा',
+                    data: data.map(item => item.AvailableLeave),
+
                 }
             ]
         },
