133c7cebf3cde00b296542c5c45fc24643b1b87d
dinesh bajgain <bajgaindinesh@gmail.com>
2024-01-11 09:28:02 +0545

    changes in super admin and fav icon

--- AttendanceManagementSystem/Scripts/Menu/customChart.js
+++ AttendanceManagementSystem/Scripts/Menu/customChart.js
@@ -47,55 +47,33 @@
         }
     });
 }
-var count = 0
 function loadSuperAdminChartData(chartData) {
-    count = count+1
-    console.log(count)
-    const data = chartData.map(res => ({ LeaveTitle: res.LeaveTitle, RemainingLeave: res.RemainingLeave, AvailableLeave: res.AvailableLeave }));
-    const chartLabels = chartData.map(res => res.LeaveTitle)
+    const doughnutLabels = [
+        'Red',
+        'Blue',
+        'Yellow'
+    ];
+    const doughnutData = [300, 50, 100]
     const ctx = document.getElementById('superadminChart');
     new Chart(ctx, {
         height: 30,
         width: 100,
-        type: 'bar',
+        type: 'doughnut',
         data: {
-            labels: chartLabels,
-            datasets: [
-                {
-                    borderWidth: 2,
-                    label: 'बाँकी बिदा',
-                    backgroundColor: "rgba(246, 39, 53, 1)",
-                    data: data.map(item => item.RemainingLeave),
-
-                },
-                {
-                    borderWidth: 2,
-                    backgroundColor: "rgba(254, 193, 32, 1)",
-                    label: 'कुल बिदा',
-                    data: data.map(item => item.AvailableLeave),
-
-                }
-            ]
+            labels: doughnutLabels,
+            datasets: [{
+                label: 'My First Dataset',
+                data: [300, 50, 100],
+                backgroundColor: [
+                    'rgb(255, 99, 132)',
+                    'rgb(54, 162, 235)',
+                    'rgb(255, 205, 86)'
+                ],
+                hoverOffset: 4
+            }]
         },
         options: {
             maintainAspectRatio: false,
-            responsive: !0,
-            barThickness: 35,
-            plugins: {
-                legend: {
-                    display: !1
-                }
-            },
-            scales: {
-                xAxes: [{
-                    gridLines: {
-                        color: "rgba(0, 0, 0, 0)"
-                    }
-                }],
-                y: {
-                    min: 0
-                }
-            }
         }
-    });
+   });
 }
